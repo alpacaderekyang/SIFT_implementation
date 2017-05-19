639da1b4da660d6f830bde08a35257ba7640cc68
derek <derek@DESKTOP-CAUER8V>
2017-05-19 16:57:17 +0800

    count feature points

--- SiftMain.m
+++ SiftMain.m
@@ -212,48 +212,75 @@ end
 imgtemp4 = img*0.5;
 figure,imshow(uint8(imgtemp4*255));
 for i = 1:3
-for j = 1:3
-for k = 1:2
-if isempty(DominantOrientation{i,j})
-continue
-end
-DomOri = DominantOrientation{i,j}(:,:,k);
-DOx = cos(DomOri(:,1)).*DomOri(:,2)*200;
-DOy = sin(DomOri(:,1)).*DomOri(:,2)*200;
-Map = DiffMinMaxMapNoEdge{i,j};
-SSCindx = find(Map); % Scale-Space-Corner Index
-for ssc = 1:length(SSCindx)
-    hold on;
-    [Row,Col] = ind2sub([size(Map,1),size(Map,2)], SSCindx(ssc));
-    Row = Row*2^(i-1)+1; Col = Col*2^(i-1)+1; % offset = [1,1];
-    if (Row > size(img,1)) || (Col > size(img,2))
-        continue
+    for j = 1:3
+        for k = 1:2
+            if isempty(DominantOrientation{i,j})
+                continue
+            end
+            DomOri = DominantOrientation{i,j}(:,:,k);
+            DOx = cos(DomOri(:,1)).*DomOri(:,2)*200;
+            DOy = sin(DomOri(:,1)).*DomOri(:,2)*200;
+            Map = DiffMinMaxMapNoEdge{i,j};
+            SSCindx = find(Map); % Scale-Space-Corner Index
+            for ssc = 1:length(SSCindx)
+                hold on;
+                [Row,Col] = ind2sub([size(Map,1),size(Map,2)], SSCindx(ssc));
+                Row = Row*2^(i-1)+1; Col = Col*2^(i-1)+1; % offset = [1,1];
+                if (Row > size(img,1)) || (Col > size(img,2))
+                    continue
+                end
+                quiver(Col,Row,DOx(ssc),DOy(ssc),'r')
+            end
+        end
     end
-    quiver(Col,Row,DOx(ssc),DOy(ssc),'r')
-end
-end
-end
 end
 hold off;
 %
+
 imgtemp5 = img*0.5;
 for i = 1:3
-for j = 1:3
-for k = 1:2
-if isempty(DominantOrientation{i,j})
-continue
-end
-Map = DiffMinMaxMapNoEdge{i,j};
-SSCindx = find(Map); % Scale-Space-Corner Index
-for ssc = 1:length(SSCindx)
-    [Row,Col] = ind2sub([size(Map,1),size(Map,2)], SSCindx(ssc));
-    Row = Row*2^(i-1)+1; Col = Col*2^(i-1)+1; % offset = [1,1];
-    if (Row > size(img,1)) || (Col > size(img,2))
-        continue
+    for j = 1:3
+        for k = 1:2
+            if isempty(DominantOrientation{i,j})
+                continue
+            end
+            Map = DiffMinMaxMapNoEdge{i,j};
+            SSCindx = find(Map); % Scale-Space-Corner Index
+            for ssc = 1:length(SSCindx)
+                [Row,Col] = ind2sub([size(Map,1),size(Map,2)], SSCindx(ssc));
+                Row = Row*2^(i-1)+1; Col = Col*2^(i-1)+1; % offset = [1,1];
+                if (Row > size(img,1)) || (Col > size(img,2))
+                    continue
+                end
+                imgtemp5(Row,Col) = 1;
+            end
+        end
     end
-    imgtemp5(Row,Col) = 1;
-end
-end
-end
 end
 figure,imshow(uint8(imgtemp5*255));
+
+%-------------------------------------------
+imgtemp6 = img*0.5; %only for lower brightness
+FeatureCount = 0;
+for i = 1:3
+    for j = 1:3
+        for k = 1:2
+            if isempty(DominantOrientation{i,j})
+                continue
+            end
+            Map = DiffMinMaxMapNoEdge{i,j};
+            SSCindx = find(Map); % Scale-Space-Corner Index
+            for ssc = 1:length(SSCindx)
+                [Row,Col] = ind2sub([size(Map,1),size(Map,2)], SSCindx(ssc));
+                Row = Row*2^(i-1)+1; Col = Col*2^(i-1)+1; % offset = [1,1];
+                if (Row > size(img,1)) || (Col > size(img,2))
+                    continue
+                end
+                FeatureCount = FeatureCount + 1;
+                imgtemp5(Row,Col) = 1;
+            end
+        end
+    end
+end
+figure,imshow(uint8(imgtemp6*255));
+fprintf('Number of feature points... %f\n',FeatureCount);
